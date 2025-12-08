d6007c31bb9fecf9a3601b79c07658b887d56890
HarrisWarner04 <connect.harshawardhan@gmail.com>
2025-12-09 03:46:58 +0530

    Prepare backend for Render deployment

--- backend/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Build script for Render deployment
-# Forces use of pre-built wheels to avoid Rust compilation
-
-pip install --upgrade pip
-pip install --only-binary :all: -r requirements.txt || pip install -r requirements.txt
-
